b0f84ddc8196348e00a8111fc59239aa26e10d85
DaeseongKim <eotjd1105@icloud.com>
2021-04-30 11:35:46 +0900

    [user][signup] /user/signup 엔드포인트 구현 및 데이터베이스 수정 1. DB Users 테이블에 Salt Column 추가 2. signin, signup 패스워드 hash logic 추가

--- controllers/users.js
+++ controllers/users.js
@@ -22,20 +22,14 @@ module.exports = {
           email: email,
         },
       });
+
       // ! DB에서 email로 user 조회 후, 그 user의 salt와 req.body의 password를 통해 hashedPassword 조회
       // ! 그 hashedPassword가 DB의 Password와 같다면 로그인 성공 그렇지 않으면 잘못되었다고 전송
-      // const hashedPw = crypto.pbkdf2Sync(password, userData[0].salt, 103523, 64, 'sha512').toString('base64')
+      const hashedPassword = crypto
+        .pbkdf2Sync(password, userData.salt, 103523, 64, "sha512")
+        .toString("base64");
 
-      // if(hashedPw !== userData[0].password) {
-      //   res.status(403).json({ message: "Invalid user email or password" });
-      // } else {
-      //   const accessToken = generateAccessToken(userData.dataValues);
-      //   const refreshToken = generateRefreshToken(userData.dataValues);
-      //   sendRefreshToken(res, refreshToken);
-      //   sendAccessToken(res, 200, accessToken);
-      // }
-      // ! hash 안 썼을 때
-      if (!userData) {
+      if (hashedPassword !== userData.password) {
         res.status(403).json({ message: "Invalid user email or password" });
       } else {
         const accessToken = generateAccessToken(userData.dataValues);
@@ -50,34 +44,27 @@ module.exports = {
   signup: async (req, res) => {
     try {
       const { email, password, username } = req.body;
-
       const userData = await User.findOne({
         where: { email: email },
       });
+      if (!email || !password || !username) {
+        throw err;
+      }
       if (userData) {
         res.status(409).json({
           message: "email exists",
         });
       } else {
         // ! hash로 암호화해서 hashedPassword/ Salt 모두 저장
-        // const salt = crypto.randomBytes(64).toString();
-        // const hashedPassword = crypto
-        //   .pbkdf2Sync(password, salt, 103523, 64, "sha512")
-        //   .toString("base64");
-        // await User.create({
-        //   name: username,
-        //   email: email,
-        //   password: hashedPassword,
-        //   salt: salt,
-        // });
-        // res.status(201).json({
-        //   message: "Signed up successfully",
-        // });
-
+        const salt = crypto.randomBytes(64).toString("base64");
+        const hashedPassword = crypto
+          .pbkdf2Sync(password, salt, 103523, 64, "sha512")
+          .toString("base64");
         await User.create({
           name: username,
           email: email,
-          password: password,
+          password: hashedPassword,
+          salt: salt,
         });
         res.status(201).json({
           message: "Signed up successfully",
