62b4555441e2eff6c64a1ab5c18af74253e45eee
DaeseongKim <eotjd1105@icloud.com>
2021-05-03 14:43:41 +0900

    [crop][info] 엔드포인트 수정 1. 데이터 응답 방식 변경

--- controllers/crop/info.js
+++ controllers/crop/info.js
@@ -1,5 +1,5 @@
 const { isAuthorized } = require("../token");
-const { Crop, Seed } = require("../../models");
+const { Crop, Seed, Kind } = require("../../models");
 const { sendStatAndData, sendStatAndMsg } = require("../actions");
 
 module.exports = async (req, res) => {
@@ -9,12 +9,28 @@ module.exports = async (req, res) => {
   }
   try {
     let data = await Crop.findAll({
-      attributes: ["id", "name"],
+      attributes: [["id", "crops_id"], "name"],
+      include: [
+        {
+          model: Kind,
+          attributes: ["icon"],
+        },
+        {
+          model: Seed,
+          attributes: [["id", "seed_id"], ["name", "seed_name"], "isHarvested"],
+        },
+      ],
       where: { farms_id: req.params.farmid },
-      include: [{ model: Seed, attributes: ["id", "name", "isHarvested"] }],
     });
-    const cropData = data.map((el) => el.get({ plain: true }));
-    sendStatAndData(res, 200, cropData);
+    data = data.map((el) => el.get({ plain: true }));
+    const revised = data.map((crop) => {
+      let obj = {
+        ...crop,
+        Kind: crop.Kind.icon,
+      };
+      return obj;
+    });
+    sendStatAndData(res, 200, revised);
     return;
   } catch (err) {
     sendStatAndMsg(res, 404, "Not Found");
